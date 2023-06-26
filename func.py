9210e87958fd32607152a7f991213649b1556973
Fotis Voutsas <fotis@netdata.cloud>
2023-06-26 17:47:44 +0300

    Update func.py

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import csv
+
 # import yaml
 import os
 import ruamel.yaml
@@ -8,100 +9,96 @@ import yaml as yl
 from ruamel.yaml import YAML, representer
 from collections import OrderedDict
 
+
 class NoAliasDumper(ruamel.yaml.RoundTripRepresenter):
     def ignore_aliases(self, data):
         return True
 
+module_count = 0
 def scrape_alerts():
     dict = {}
-    dir="./health/health.d"
-    for file in next(os.walk(f'{dir}'))[2]:
+    dir = "./health/health.d"
+    for file in next(os.walk(f"{dir}"))[2]:
         if file.endswith(".conf"):
-            filename=file
-            file = open(f'{dir}/{file}', "r")
+            filename = file
+            file = open(f"{dir}/{file}", "r")
 
             whole_file = file.readlines()
-            i=0
-            name=""
-            
-            for line in whole_file:
+            i = 0
+            name = ""
+
+            for index, line in enumerate(whole_file):
                 nest_dict = {}
-                i+=1
+                i += 1
+                # print("\n\nLINE:", line.strip("\n"))
                 if not line.startswith("#"):
                     # print("Line is",line)
                     if "template:" in line or "alarm:" in line:
                         # print("Line", line)
-                        name = line.split(": ")[1].strip("\n")
+                        name = line.split(": ",1)[1].strip("\n")
                         # print(name)
                     if "on:" in line:
-                        metric = line.split(": ")[1].strip("\n")
+                        metric = line.split(": ",1)[1].strip("\n")
                         # print(metric)
                     if "info:" in line:
-                        info = line.split(": ")[1].strip("\n")
-                        # print(info)
-                    # if "os:" in line:
-                    #     operating_sys= line.split(": ")[1].strip("\n")
-                    # # print(i, len(whole_file))
-                    if name and (line == "\n" or i==len(whole_file)-1):
-                        try:
+                        # line= line.replace("\\", "ABC")
+                        # print(line, line.endswith("\\\n"))
+                        info = line.split(": ",1)[1].strip("\n").strip("\\")
+                        i=1
+                        while line.endswith("\\\n"):
+                          # print("in")
+                          line = whole_file[index+i]
+                          i+=1
+                          info += line.strip("\\\n").lstrip()
+                          # print(line)
+                          # return
+                        # print(info,"\n")
+                    if "os:" in line:
+                        opsys = line.split(": ",1)[1].strip("\n")
 
+                    if name and (line == "\n" or i == len(whole_file) - 1):
+                        try:
                             # nest_dict = {"name": name, "metric": metric, "os": operating_sys, "info": info}
-                            nest_dict = {"name": f"[{name}](https://gihtub.com/netdata/netdata/blob/master/health/health.d/{filename})", "metric": metric, "info": info}
-                            
+                            nest_dict = {
+                                "name": f"[{name}](https://gihtub.com/netdata/netdata/blob/master/health/health.d/{filename})",
+                                "metric": metric,
+                                "info": info,
+                                "os": opsys
+                            }
+
                             try:
                                 dict[metric]
                             except:
                                 dict[metric] = []
 
-
-
                             dict[metric].append(nest_dict)
                             # print("OVER", dict)
 
-
+                            name = None
+                            info = None
 
                         except Exception as e:
-                            print("Exception",e)
-            # print(dict)
-            # exit()
-            # dummy_file.close()
-
-
-            # if whole_file.startswith("<!--"):
-            #     body = whole_file.split("-->", 1)[1]
-            # else:
-            #     body = whole_file
-
-            # dummy_file = open(path_to_file, "w")
-            # dummy_file.seek(0)
-            # dummy_file.write(output + body)
-            # dummy_file.close()
+                            print("Exception", e)
     return dict
 
+
 def csv_to_yaml(csv_file, yaml_file, alert_dict):
-
-        
-
     yaml_string = """
 name: Collector name
 title: COLLECTORNAME Monitoring
 duplicate_for_virtual_integrations:
-  list:
-    - title: Ubuntu
-      description: dummy description that will replace the original "overview" section, leave the whole list empty if none
-    - title: Redhat
-      description: dummy description that will replace the original "overview" section, leave the whole list empty if none
+  list: ["[Ubuntu](link to ubuntu)", "[Redhat](link to redhat)"]
 overview:
   operation:
-    description: This collector periodically does http requests to one or more [Apache](link to collector) web servers to collect the metrics mod_status plugin that apache exposes
+    description: This collector periodically does http requests to one or more {[Apache](link to collector)} (<- CI will remove the curly brackets, they are for replacing with the virtual integrations, to replace the link each time) web servers to collect the metrics mod_status plugin that apache exposes
   platforms:
-    description: If this collector can't run on some platforms specify them here, otherwise set this to true and this text will not be rendered, instead a templated message will appear
-    boolean: true
+    description: If this collector can't run on some platforms specify them here and set it to false, otherwise set this to true and this text will not be rendered, instead a templated message will appear
+    use_templated_text: true
   multi-instance:
     boolean: true
   permissions:
     description: if there are any notable permission requirements, eg running as root and so on, specify them here and set it to false, otherwise set it to true and a templated message will appear
-    boolean: true
+    use_templated_text: true
   related:
     description: "give a simple description if there are any other collectors related to this collector. Example -> To get the most out of your apache servers, you can monitor its processes for resources utilization and their log files to turn them into real-time metrics. Check our related resources for more information. (if not, leave empty)"
 setup:
@@ -154,102 +151,119 @@ related_resources:
 """
 
     template = yl.safe_load(yaml_string)
-
-    
+    global module_count
     metricslist = []
     data = {}
-    data['metrics'] = {}
-    data['metrics'] = {}
-    
+    data["metrics"] = {}
+    # data['metrics'] = {}
+
     with open(csv_file) as f:
         csv_data = csv.reader(f)
         next(csv_data)  # Skip the header
         metric = {}
         alerts = {}
-        
-        modules = pd.unique(pd.read_csv(csv_file)["module"])
-        # return
-        for row in csv_data:
 
+        modules = pd.unique(pd.read_csv(csv_file)["module"])
+        # print(modules)
+        # return
+
+        for row in csv_data:
+            name = row[0]
+            description = row[4]
+            unit = row[3]
+            plugin = row[7]
+            dimensions = [{"name": n} for n in [x.strip() for x in row[2].split(",") if x]]
             scope = row[1]
             if not scope:
                 scope = "global"
-            
+
             module = row[8]
             if not module:
                 module = row[7]
 
             if module not in metric:
-                metric[module]={}
+                # print(module, "Not in dict", name)
+                metric[module] = {}
+
             if scope not in metric[module]:
-                labels = [{"name": x.strip(), "description": "TBD"} for x in row[6].split(',') if x]
+                labels = [
+                    {"name": x.strip(), "description": "TBD"}
+                    for x in row[6].split(",")
+                    if x
+                ]
                 # for item in labels:
                 #     label
                 # metric[scope]= {"name":scope, "description":"TBD", "labels": labels, "metrics":[]}
-                
-                metric[module][scope]= {"name":scope, "description":"TBD", "labels": labels, "metrics":[]}
+
+                metric[module][scope] = {
+                    "name": scope,
+                    "description": "TBD",
+                    "labels": labels,
+                    "metrics": [],
+                }
                 # print(scope, metric.keys(), scope in metric.keys(), scope in metric)
-            
-            name = row[0]
-            description = row[4]
-            unit = row[3]
-            dimensions = [{"name": n} for n in [x.strip() for x in row[2].split(',') if x] ]
-            metric[module][scope]["metrics"].append({"name":name,"description":description,"unit":unit,"dimensions":dimensions})
-            
+
+            metric[module][scope]["metrics"].append(
+                {
+                    "name": name,
+                    "description": description,
+                    "unit": unit,
+                    "dimensions": dimensions,
+                }
+            )
+
             try:
                 alerts[module]
             except:
                 alerts[module] = []
-            
+
             try:
                 alerts[module] += alert_dict[name]
-                print(module, "Module has alerts")
             except Exception as e:
-                print("Exception" , e)
-            
-            plugin = row[7]
+                pass
+                # print("No alerts" , e)
 
             # print(name)
             # if name.startswith("system"):
             #     print(name, plugin)
             #     print(alert_dict[name])
-        
-        print("Module length",len(modules))
+
+        # print("Module length", len(modules))
         try:
             if pd.isnull(modules.any()):
-                print("it is null")
+                # print("it is null")
                 modules = [f"{plugin}"]
         except:
             pass
         try:
             if np.isnan(modules):
-                print("it is null")
+                # print("it is null")
                 modules = [f"{plugin}"]
         except:
             pass
-        
-        
 
-
-        
         for module in modules:
-            print("Module", module)
+            module_count+=1
+            # print("Module", module)
             # metricslist.append({"name": module ,"metrics":{"folding":{"title": "Metrics", "enabled":False},"scope":metric[module]}})
 
-            
-
             template["alerts"] = alerts[module]
-            template['metrics'] = ({"description":"some description for this section, or leave empty","folding":{"title": "Metrics", "enabled":False},"scope":metric[module]})
+            template["metrics"] = {
+                "description": "some description for this section, or leave empty",
+                "folding": {"title": "Metrics", "enabled": False},
+                "scope": metric[module],
+            }
             template["name"] = module
-            metricslist.append(template)
-            print(template.keys())
+            metricslist.append(template.copy())
+            # print(template)
+            # print(metricslist[len(metricslist)-1])
+            # print(metricslist)
             # metricslist.append({
-                                
+
             #                     "alerts": alerts[module],
 
-
             #                     "metrics":{"description":"some description for this section, or leave empty","folding":{"title": "Metrics", "enabled":False},"scope":metric[module]}})
-        print("end of loop")
+        # print("end of loop")
 
         # dcit = {"name": module ,
         #                         "title": "TBD",
@@ -258,16 +272,14 @@ related_resources:
         #                         "configuration": {"options": {"desription": "TBD, use | and newline for multiline","folding":{"title": "Config options", "enabled": True}, "list": [{"name": "option name", "description": "option description", "default":"a number or a string", "required": "true or false"}]}, "examples": {"folding":{"title": "Config", "enabled": False},"list": [{"name": "Basic / else describe what this example is about", "description": "A basic example configuration / describe your example", "data": "input the example data in yaml format"}]}},
         #                         "troubleshooting": {"problems": {"list": [{"name": "possible troubleshooting title", "text": "describe"}]}}}
 
-
         if len(modules) > 1:
-            print("multi", len(modules))
-            finaldata = {"name": plugin,"title":"TBD", "modules": metricslist}
-            
-            
-            with open(yaml_file, 'w') as yf:
-            # yaml.dump(data, yf, default_flow_style=False, sort_keys=False)
+            # print("multi", len(modules))
+            finaldata = {"name": plugin, "title": "TBD", "modules": metricslist}
 
-                yaml=YAML()
+            with open(yaml_file, "w") as yf:
+                # yaml.dump(data, yf, default_flow_style=False, sort_keys=False)
+
+                yaml = YAML()
                 yaml.default_flow_style = False
                 yaml.Representer = NoAliasDumper
                 yaml.sort_keys = False
@@ -275,13 +287,13 @@ related_resources:
                 # yaml.indent(mapping=2,sequence=4)
                 yaml.dump(finaldata, yf)
         else:
-            print("single")
+            # print("single")
             finaldata = metricslist
 
-            with open(yaml_file, 'w') as yf:
-            # yaml.dump(data, yf, default_flow_style=False, sort_keys=False)
+            with open(yaml_file, "w") as yf:
+                # yaml.dump(data, yf, default_flow_style=False, sort_keys=False)
 
-                yaml=YAML()
+                yaml = YAML()
                 yaml.default_flow_style = False
                 yaml.Representer = NoAliasDumper
                 yaml.sort_keys = False
@@ -289,33 +301,66 @@ related_resources:
                 # yaml.indent(mapping=2,sequence=4)
                 yaml.dump(finaldata[0], yf)
 
-
-
-
-
-
             # print(name, description, unit,dimensions, labels)
     # sample = []
     # for element in metric.values():
     #     sample.append(element)
 
     # data["metrics"] = metric[scope]["metrics"]
-            # print(metric)
+    # print(metric)
     # data = {"metrics":{"folding":{"title": "Metrics", "enabled":False},"description":"TBD","scope":sample}}
     # data = {"metrics":{"folding":{"title": "Metrics", "enabled":False},"scope":sample}}
     # print(metricslist)
-    
+
 
 # print([x[0] for x in os.walk("./modules")])
+# dir = "./collectors/python.d.plugin"
 dir = "./collectors"
-for directory in next(os.walk(f'{dir}'))[1]:
-    # directory = "proc.plugin"
+
+for directory in next(os.walk(f"{dir}"))[1]:
+    # directory = "freebsd.plugin"
     # print("\n"+directory)
     alert_dict = scrape_alerts()
-    # print(alert_dict)#, alert_dict["system.ram"])
     try:
-        csv_to_yaml(f'{dir}/{directory}/metrics.csv', f'{dir}/{directory}/metadata.yaml', alert_dict)  
+        csv_to_yaml(
+            f"{dir}/{directory}/metrics.csv",
+            f"{dir}/{directory}/metadata.yaml",
+            alert_dict,
+        )
+        
+    except Exception as e:
+        print("Exception", e)
+
+dir = "./collectors/python.d.plugin"
+
+for directory in next(os.walk(f"{dir}"))[1]:
+    # directory = "freebsd.plugin"
+    # print("\n"+directory)
+    alert_dict = scrape_alerts()
+    try:
+        csv_to_yaml(
+            f"{dir}/{directory}/metrics.csv",
+            f"{dir}/{directory}/metadata.yaml",
+            alert_dict,
+        )
+        
     except Exception as e:
         print("Exception", e)
     # exit()
+
+dir = "./collectors/charts.d.plugin"
+
+for directory in next(os.walk(f"{dir}"))[1]:
+    # directory = "freebsd.plugin"
+    # print("\n"+directory)
+    alert_dict = scrape_alerts()
+    try:
+        csv_to_yaml(
+            f"{dir}/{directory}/metrics.csv",
+            f"{dir}/{directory}/metadata.yaml",
+            alert_dict,
+        )
         
+    except Exception as e:
+        print("Exception", e)
+print(module_count)
