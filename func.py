0d47a78b051cb29a50ab89415afecfc7214cdd4c
Fotis Voutsas <fotis@netdata.cloud>
2023-07-14 18:18:57 +0300

    Update func.py

--- func.py
+++ func.py
@@ -1,19 +1,20 @@
 import csv
-
-# import yaml
 import os
 import ruamel.yaml
 import pandas as pd
 import numpy as np
 from ruamel.yaml import YAML, representer
 from collections import OrderedDict
+import copy
 
 
 class NoAliasDumper(ruamel.yaml.RoundTripRepresenter):
     def ignore_aliases(self, data):
         return True
 
+
 module_count = 0
+
 
 def scrape_alerts():
     dict = {}
@@ -35,26 +36,26 @@ def scrape_alerts():
                     # print("Line is",line)
                     if "template:" in line or "alarm:" in line:
                         # print("Line", line)
-                        name = line.split(": ",1)[1].strip("\n")
+                        name = line.split(": ", 1)[1].strip("\n")
                         # print(name)
                     if "on:" in line:
-                        metric = line.split(": ",1)[1].strip("\n")
+                        metric = line.split(": ", 1)[1].strip("\n")
                         # print(metric)
                     if "info:" in line:
                         # line= line.replace("\\", "ABC")
                         # print(line, line.endswith("\\\n"))
-                        info = line.split(": ",1)[1].strip("\n").strip("\\")
-                        i=1
+                        info = line.split(": ", 1)[1].strip("\n").strip("\\")
+                        i = 1
                         while line.endswith("\\\n"):
-                          # print("in")
-                          line = whole_file[index+i]
-                          i+=1
-                          info += line.strip("\\\n").lstrip()
-                          # print(line)
-                          # return
+                            # print("in")
+                            line = whole_file[index+i]
+                            i += 1
+                            info += line.strip("\\\n").lstrip()
+                            # print(line)
+                            # return
                         # print(info,"\n")
                     if "os:" in line:
-                        opsys = line.split(": ",1)[1].strip("\n")
+                        opsys = line.split(": ", 1)[1].strip("\n")
 
                     if name and (line == "\n" or i == len(whole_file) - 1):
                         try:
@@ -64,7 +65,7 @@ def scrape_alerts():
                                 "link": f"https://github.com/netdata/netdata/blob/master/health/health.d/{filename}",
                                 "metric": metric,
                                 "info": info,
-                                "os": opsys
+                                "os": ruamel.yaml.scalarstring.DoubleQuotedScalarString(opsys)
                             }
 
                             try:
@@ -84,94 +85,23 @@ def scrape_alerts():
 
 
 def csv_to_yaml(csv_file, yaml_file, alert_dict):
-    yaml_string = """
-name: Collector name
-title: TBD Monitoring
-duplicate_for_virtual_integrations:
-  list:
-    - "[TBD](link to TBD)"
-    - "[TBD](link to TBD)"
-overview:
-  how_the_collector_works:
-        # The CI will remove the curly brackets in the link, they are for replacing with the virtual integrations, to replace the link each time
-    description: |
-        This collector works by... {[TBD](link to TBD)} ...
-  supported_platforms:
-    description: |
-        If this collector can't run on some platforms specify them here. Leave empty otherwise.
-  multi-instance: true
-  permissions:
-    description: |
-        if there are any notable permission requirements, eg running as root and so on, specify them here. Leave empty otherwise.
-setup:
-  prerequisites:
-    list:
-      - title: Prerequisite title for heading
-        text: |
-            - TBD
-            - TBD
-  behavior:
-    auto_detection:
-      description: |
-        TBD
-    limits:
-      description: |
-        TBD
-    performance_impact:
-      description: |
-        TBD
-  configuration:
-    file:
-      description: |
-        which file the user has to edit. Example: go.d/apache.conf
-    options:
-      description: |
-        TBD, use | and newline for multiline
-      folding:
-        title: Config options
-        enabled: true
-      list:
-        - name: option name
-          description: option description
-          default: a number or a string
-          required: true or false
-    examples:
-      folding:
-        title: Config
-        enabled: false
-      list:
-        - name: Basic / else describe what this example is about
-          description: |
-            A basic example configuration / describe your example
-          data:
-            jobs:
-                - name: example
-                  TBD: TBD
-troubleshooting:
-  problems:
-    list:
-      - name: possible troubleshooting title
-        text: |
-            describe the issue and the troubleshooting process
-related_resources:
-  integrations:
-    list: [apache, weblog, httpcheck, etc]
-        # This text will be added in the overview section
-    overview_text: |
-        give a simple description if there are any other collectors related to this collector. Example -> To get the most out of TBD, you can do XYZ with collectors X Y and Z. Check our related resources for more information.
-    provide:
-      description: |
-        What info I can provide for other plugins that reference me? this will only be visible in their end, they will scrape this text. Example -> XYZ allows monitoring X Y Z metrics...
-"""
-    yaml = ruamel.yaml.YAML()
-    template = {}
+    yaml = ruamel.yaml
+
+    with open("collectors/metadata/single-module-template.yaml") as stream:
+        try:
+            template =yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    
+
+    
 
     global module_count
-    
+
     metricslist = []
     data = {}
     data["metrics"] = {}
-    # data['metrics'] = {}
 
     with open(csv_file) as f:
         csv_data = csv.reader(f)
@@ -183,6 +113,8 @@ related_resources:
         # print(modules)
         # return
 
+
+
         for row in csv_data:
             name = row[0]
             description = row[4]
@@ -193,6 +125,8 @@ related_resources:
             scope = row[1]
             if not scope:
                 scope = "global"
+            
+
 
             module = row[8]
             if not module:
@@ -210,7 +144,7 @@ related_resources:
 
                 metric[module][scope] = {
                     "name": scope,
-                    "description": "",
+                    "description": ruamel.yaml.scalarstring.DoubleQuotedScalarString(""),
                     "labels": labels,
                     "metrics": [],
                 }
@@ -219,16 +153,16 @@ related_resources:
             metric[module][scope]["metrics"].append(
                 {
                     "name": name,
-                    "availability": [],
+                    # "availability": [],
                     "description": description,
-                    "unit": unit,
+                    # this double quoted thing ensures we got "" instead of ''
+                    "unit": ruamel.yaml.scalarstring.DoubleQuotedScalarString(unit),
                     "chart_type": chart_type,
                     "dimensions": dimensions,
                 }
             )
             # print(type(list(metric[module][scope])))
 
-            
             try:
                 alerts[module]
             except:
@@ -246,59 +180,64 @@ related_resources:
             #     print(alert_dict[name])
             # metric[module][scope] = list(metric[module][scope])
         # print("Module length", len(modules))
+
+        #TODO figure out this if therer is a module and a global row
         try:
-            if pd.isnull(modules.any()):
-                # print("it is null")
+            if pd.isnull(modules.all()):
+                # print("it is null", yaml_file)
                 modules = [f"{plugin}"]
         except:
             pass
         try:
             if np.isnan(modules):
-                # print("it is null")
+                # print("it is nan", yaml_file)
                 modules = [f"{plugin}"]
         except:
             pass
+        
+        
 
         for module in modules:
-            module_count+=1
-            # print("Module", module)
-            # metricslist.append({"name": module ,"metrics":{"folding":{"title": "Metrics", "enabled":False},"scope":metric[module]}})
-            scope_array = []
-            for key in metric[module]:
-                scope_array.append(metric[module][key])
+            try:
+                # print(module)
+                module_count += 1
+                # print("Module", module)
+                # metricslist.append({"name": module ,"metrics":{"folding":{"title": "Metrics", "enabled":False},"scope":metric[module]}})
+                scope_array = []
+                for key in metric[module]:
+                    scope_array.append(metric[module][key])
 
-            template["module_name"] = module
-            template["plugin_name"] = plugin
-            template["alerts"] = alerts[module]
-            template["metrics"] = {
-                "folding": {"title": "Metrics", "enabled": False},
-                "description": "",
-                "availability": [],
-                "scopes": scope_array,
-            }
-            metricslist.append(template.copy())
-            # print(template)
-            # print(metricslist[len(metricslist)-1])
-            # print(metricslist)
-            # metricslist.append({
+                dummy_template={}
+                dummy_template = template.copy()
 
-            #                     "alerts": alerts[module],
+                dummy_template["meta"]["module_name"] = module
+                dummy_template["meta"]["plugin_name"] = plugin
 
-            #                     "metrics":{"description":"some description for this section, or leave empty","folding":{"title": "Metrics", "enabled":False},"scope":metric[module]}})
-        # print("end of loop")
 
-        # dcit = {"name": module ,
-        #                         "title": "TBD",
-        #                         "overview": {"application": {"description": "TBD, use | and newline for multiline"},"collector": {"description": "TBD, use | and newline for multiline"}},
-        #                         "setup": {"prerequisites": {"list": [{"title": "Prerequisite title for heading", "text": "List like with dashes, TBD, use | and newline for multiline"}]}},
-        #                         "configuration": {"options": {"desription": "TBD, use | and newline for multiline","folding":{"title": "Config options", "enabled": True}, "list": [{"name": "option name", "description": "option description", "default":"a number or a string", "required": "true or false"}]}, "examples": {"folding":{"title": "Config", "enabled": False},"list": [{"name": "Basic / else describe what this example is about", "description": "A basic example configuration / describe your example", "data": "input the example data in yaml format"}]}},
-        #                         "troubleshooting": {"problems": {"list": [{"name": "possible troubleshooting title", "text": "describe"}]}}}
+                if plugin == module:
+                    dummy_template["meta"]["monitored_instance"]["name"] = plugin.replace(".plugin", "")
+                else:
+                    dummy_template["meta"]["monitored_instance"]["name"] = plugin.replace(".plugin", "") + " " + module
+
+                dummy_template["alerts"] = alerts[module]
+                dummy_template["metrics"] = {
+                    "folding": {"title": "Metrics", "enabled": False},
+                    "description": ruamel.yaml.scalarstring.DoubleQuotedScalarString(""),
+                    "availability": [],
+                    "scopes": scope_array,
+                }
+                # print(dummy_template["meta"]["module_name"])
+                metricslist.append(copy.deepcopy(dummy_template))
+            except Exception as e:
+                print(e)
+        
+        # print(metricslist)
 
         if len(modules) > 1:
             # print("multi", len(modules))
             finaldata = {"name": plugin, "modules": metricslist}
 
-            with open(yaml_file, "w") as yf:
+            with open(yaml_file.replace("metadata", "multi_metadata"), "w") as yf:
                 # yaml.dump(data, yf, default_flow_style=False, sort_keys=False)
 
                 yaml = YAML()
@@ -335,12 +274,12 @@ related_resources:
     # print(metricslist)
 
 
-print([x[0] for x in os.walk("./modules")])
+# print([x[0] for x in os.walk("./modules")])
 dir = "./collectors"
 
 for directory in next(os.walk(f"{dir}"))[1]:
-    # directory = "proc.plugin"
     # print("\n"+directory)
+    # directory = "freeipmi.plugin"
     alert_dict = scrape_alerts()
     try:
         csv_to_yaml(
@@ -348,9 +287,9 @@ for directory in next(os.walk(f"{dir}"))[1]:
             f"{dir}/{directory}/metadata.yaml",
             alert_dict,
         )
-        
+
     except Exception as e:
-        print("Exception", e)
+        print("Exception", e, directory)
     # break
 
 dir = "./collectors/python.d.plugin"
@@ -365,9 +304,9 @@ for directory in next(os.walk(f"{dir}"))[1]:
             f"{dir}/{directory}/metadata.yaml",
             alert_dict,
         )
-        
+
     except Exception as e:
-        print("Exception", e)
+        print("Exception", e, directory)
     # break
     # exit()
 
@@ -383,7 +322,30 @@ for directory in next(os.walk(f"{dir}"))[1]:
             f"{dir}/{directory}/metadata.yaml",
             alert_dict,
         )
-        
+
     except Exception as e:
-        print("Exception", e)
+        print("Exception", e, directory)
+
 print(module_count)
+
+
+
+
+
+# # print([x[0] for x in os.walk("./modules")])
+# dir = "../go.d.plugin/modules"
+
+# for directory in next(os.walk(f"{dir}"))[1]:
+#     directory = "chrony"
+#     # print("\n"+directory)
+#     alert_dict = scrape_alerts()
+#     try:
+#         csv_to_yaml(
+#             f"{dir}/{directory}/metrics.csv",
+#             f"{dir}/{directory}/metadata.yaml",
+#             alert_dict,
+#         )
+
+#     except Exception as e:
+#         print("Exception", e)
+#     # break
